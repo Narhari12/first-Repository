4d19369e01e302e3d64b4944cff44cfb740dcad2
Narhari12 <narhari.crm@gmail.com>
2021-02-09 11:14:37 +0530

    first commit

---
+++ AIfElse.java
@@ -0,0 +1,18 @@
+public class AIfElse
+{
+ public static void main(String args [])
+{
+ int i = 20;
+
+if(i>11)
+{
+ System.out.println("Given no i = "+i+"\t" +"is Greater than 11");
+} 
+else
+{
+ System.out.println("Given no i = "+i+"\t" +"is Less than 11");
+}
+
+}
+
+}
